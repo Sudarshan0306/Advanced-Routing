8cf32fc1f7a579c07ff6f63a418ad90e17528c62
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-07 22:45:50 +0530

    useRouteError hook

---
+++ src/pages/Error.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+import PageContent from "./PageContent";
+import MainNavigation from "../components/MainNavigation";
+
+const Error = () => {
+  const error = useRouteError();
+  let title = "An Error Occured";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = JSON.parse(error.data).message;
+  }
+  if (error.status === 404) {
+    message = "Could not find the source or the page";
+  }
+
+  return (
+    <>
+      <MainNavigation />
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
+    </>
+  );
+};
+
+export default Error;
