d97d1961c6544299389070cfa0736cd83b035113
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-08 21:34:32 +0530

    json function of react-router

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,5 +1,5 @@
 import EventsList from "../components/EventsList";
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData();
@@ -17,9 +17,10 @@ export default EventsPage;
 export const loader = async () => {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
-    throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events" }), {
+    //   status: 500,
+    // });
+    throw json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     return response;
   }
