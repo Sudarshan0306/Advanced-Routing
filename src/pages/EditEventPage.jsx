bd85fa13edd0912e21a31e1f4a7409f7e433fcf9
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-08-10 18:52:20 +0530

    used useRouteLoaderData

--- src/pages/EditEventPage.jsx
+++ src/pages/EditEventPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import EventForm from '../components/EventForm';
-import { useLoaderData } from 'react-router-dom';
+import { useRouteLoaderData } from 'react-router-dom';
 
 const EditEventPage = () => {
-  const data = useLoaderData()
+  const data = useRouteLoaderData('event-detail');
+
   return (
     <>
       <EventForm event={data.event}/>
